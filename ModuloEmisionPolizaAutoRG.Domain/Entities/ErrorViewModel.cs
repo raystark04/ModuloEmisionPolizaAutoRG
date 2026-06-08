0d5ec03f0bfe8bef7b66c20f84febf37b9dc3ab3
Raymond García <raymondgarcia1019@gmail.com>
2026-06-07 22:39:43 -0600

    Delete ModuloEmisionPolizaAutoRG.Domain directory

--- ModuloEmisionPolizaAutoRG.Domain/Entities/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ModuloEmisionPolizaAuto.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- ModuloEmisionPolizaAutoRG.Domain/Entities/PolizaCobertura.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ModuloEmisionPolizaAuto.Models
-{
-    public class PolizaCobertura
-    {
-        public int Id { get; set; }
-        public int PolizaId { get; set; }
-        public int CoberturaId { get; set; }
-    }
-}
